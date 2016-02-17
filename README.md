1f0627c503c182e9403abcec67d11ccc536910e0
riptide_wave <chris@servernetworktech.com>
2016-02-17 13:34:01 -0600

    Initial Upload
    First upload for the Z1 board

---
+++ README.md
@@ -0,0 +1,33 @@
+# Openwrt-MR18
+
+Bringup repo for the Cisco Meraki Z1 on OpenWRT!
+
+Building
+-----
+#### Build Only
+`./build.sh`
+
+#### Modify Configs and Build
+`./build.sh modify`
+
+Note that you will need to run a modify on the first compile to select the NAND image in the OpenWRT menuconfig.
+
+To Do
+-----
+##### Z1
+  * Sys Integration
+  * Kernel Device Profile
+  * Sysupgrade
+  * WiFi
+  * Ethernet
+  * LED's/GPIOs
+  * Merge upstream!
+
+Working
+-----
+##### Z1
+  * Nothing yet...
+
+Notice
+------
+No promises this won't brick your AP, and no promises that this will even work!
---
+++ overlay/target/linux/ar71xx/files/arch/mips/ath79/mach-z1.c
@@ -0,0 +1,113 @@
+/*
+ *  Cisco Meraki Z1 board support
+ *
+ *  Copyright (C) 2016 Chris Blake <chrisrblake93@gmail.com>
+ *
+ *  Based on Cisco Meraki GPL Release r23-20150601 Z1 Device Config
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+#include <linux/platform_device.h>
+#include <linux/ath9k_platform.h>
+#include <linux/platform/ar934x_nfc.h>
+
+#include <asm/mach-ath79/ath79.h>
+#include <asm/mach-ath79/ar71xx_regs.h>
+
+#include <linux/leds-nu801.h>
+#include <linux/pci.h>
+
+#include "common.h"
+#include "dev-eth.h"
+#include "pci.h"
+#include "dev-gpio-buttons.h"
+#include "dev-leds-gpio.h"
+#include "dev-nfc.h"
+#include "dev-wmac.h"
+#include "machtypes.h"
+
+/* #define Z1_GPIO_LED_POWER_WHITE	18  Still need to confirm this*/
+#define Z1_GPIO_LED_POWER_ORANGE    17
+
+#define Z1_GPIO_NU801_CKI        14
+#define Z1_GPIO_NU801_SDI        15
+
+#define Z1_GPIO_XLNA0		18
+#define Z1_GPIO_XLNA1		19
+
+#define Z1_GPIO_BTN_RESET    12
+#define Z1_KEYS_POLL_INTERVAL    20  /* msecs */
+#define Z1_KEYS_DEBOUNCE_INTERVAL  (3 * Z1_KEYS_POLL_INTERVAL)
+
+static struct gpio_led Z1_leds_gpio[] __initdata = {
+	{
+		.name = "z1:orange:power",
+		.gpio = Z1_GPIO_LED_POWER_ORANGE,
+		.active_low  = 1,
+	},
+};
+
+static struct gpio_keys_button Z1_gpio_keys[] __initdata = {
+	{
+		.desc = "reset",
+		.type = EV_KEY,
+		.code = KEY_RESTART,
+		.debounce_interval = Z1_KEYS_DEBOUNCE_INTERVAL,
+		.gpio    = Z1_GPIO_BTN_RESET,
+		.active_low  = 1,
+	},
+};
+
+static struct led_nu801_template tricolor_led_template = {
+	.device_name = "z1",
+	.name = "tricolor",
+	.num_leds = 1,
+	.cki = Z1_GPIO_NU801_CKI,
+	.sdi = Z1_GPIO_NU801_SDI,
+	.lei = -1,
+	.ndelay = 500,
+	.init_brightness = {
+		LED_OFF,
+		LED_OFF,
+		LED_OFF,
+	},
+	.default_trigger = "none",
+	.led_colors = { "red", "green", "blue" },
+};
+
+static struct led_nu801_platform_data tricolor_led_data = {
+	.num_controllers = 1,
+	.template = &tricolor_led_template,
+};
+
+static struct platform_device tricolor_leds = {
+	.name = "leds-nu801",
+	.id = -1,
+	.dev.platform_data = &tricolor_led_data,
+};
+
+static void __init z1_setup(void)
+{
+	/* NAND */
+	ath79_nfc_set_ecc_mode(AR934X_NFC_ECC_SOFT_BCH);
+	ath79_register_nfc();
+
+	/* MDIO Interface */
+	ath79_register_mdio(0, 0x0);
+
+	/* XLNA */
+	ar934x_set_ext_lna_gpio(0,Z1_GPIO_XLNA0);
+	ar934x_set_ext_lna_gpio(1,Z1_GPIO_XLNA1);
+
+	/* LEDs and Buttons */
+	platform_device_register(&tricolor_leds);
+	ath79_register_leds_gpio(-1, ARRAY_SIZE(Z1_leds_gpio),
+				 Z1_leds_gpio);
+	ath79_register_gpio_keys_polled(-1, Z1_KEYS_POLL_INTERVAL,
+					ARRAY_SIZE(Z1_gpio_keys),
+					Z1_gpio_keys);
+
+}
+MIPS_MACHINE(ATH79_MACH_Z1, "Z1", "Meraki Z1", z1_setup);
