9ba945ee0630ef1001cf7eb63e1986648b1820ac
Christian Lamparter <chunkeey@googlemail.com>
2016-02-23 22:31:38 +0100

    tools: mkmerakifw add support for older meraki firmware headers

--- overlay/tools/firmware-utils/src/mkmerakifw.c
+++ overlay/tools/firmware-utils/src/mkmerakifw.c
@@ -18,6 +18,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <libgen.h>
+#include <endian.h>
 #include <getopt.h>
 #include <errno.h>
 #include <arpa/inet.h>
@@ -35,24 +36,51 @@
 #define HDR_OFF_FILLER		36
 #define HDR_OFF_STATICHASH	40
 
+#define OLD_HDR_LENGTH		0x00000020
+#define OLD_HDR_OFF_MAGIC1	0
+#define OLD_HDR_OFF_LOAD_ADDR	4
+#define OLD_HDR_OFF_IMAGELEN	8
+#define OLD_HDR_OFF_ENTRY	12
+#define OLD_HDR_OFF_CHECKSUM	16
+#define OLD_HDR_OFF_FILLER0	20
+#define OLD_HDR_OFF_FILLER1	24
+#define OLD_HDR_OFF_FILLER2	28
+
+enum header_type_t {
+	MERAKI_OLD,
+	MERAKI_NEW,
+};
+
 struct board_info {
-	uint32_t magic;
-	uint32_t imagelen;
-	unsigned char statichash[20];
+	enum header_type_t header_type;
 	char *id;
 	char *description;
+	uint32_t magic;
+	uint32_t imagelen;
+
+	union {
+		struct {
+			unsigned char statichash[20];
+		};
+		struct {
+			uint32_t load_addr;
+			uint32_t entry;
+		};
+	};
 };
 
 /*
  * Globals
  */
 static char *progname;
+static bool strip_padding;
 
 static char *board_id;
 static const struct board_info *board;
 
 static const struct board_info boards[] = {
 	{
+		.header_type	= MERAKI_NEW,
 		.id		= "mr18",
 		.description	= "Meraki MR18 Access Point",
 		.magic		= 0x8e73ed8a,
@@ -61,6 +89,15 @@ static const struct board_info boards[] = {
 				   0x6b, 0x4b, 0x0d, 0x32, 0x55,
 				   0xbf, 0xef, 0x95, 0x60, 0x18,
 				   0x90, 0xaf, 0xd8, 0x07, 0x09},
+	}, {
+		.header_type	= MERAKI_OLD,
+		.id		= "z1",
+		.description	= "Meraki Z1 Access Point",
+		.magic		= 0x4d495053,
+		.imagelen	= 0x00800000,
+		.load_addr	= 0x80060000,
+		.entry		= 0x80060000
+//		.entry		= 0x803460a0
 	}, {
 		/* terminating entry */
 	}
@@ -121,23 +158,188 @@ static void usage(int status)
 	exit(status);
 }
 
-void writel(unsigned char *buf, size_t offset, uint32_t value)
+static void writel(unsigned char *buf, size_t offset, uint32_t value)
 {
-	value = htonl(value);
+	value = htobe32(value);
 	memcpy(buf + offset, &value, sizeof(uint32_t));
+}
+
+static int meraki_new(const struct board_info *board, size_t klen,
+		      FILE *out, FILE *in)
+{
+	unsigned char *kernel;
+	unsigned char *buf;
+	size_t buflen;
+	size_t kspace;
+
+	size_t rc;
+	buflen = board->imagelen;
+	kspace = buflen - HDR_LENGTH;
+
+	if (klen > kspace) {
+		ERR("kernel file is too big - max size: 0x%08lX\n", kspace);
+		return EXIT_FAILURE;
+	}
+
+	/* If requested, resize buffer to remove padding */
+	if (strip_padding)
+		buflen = klen + HDR_LENGTH;
+
+	/* Allocate and initialize buffer for final image */
+	buf = malloc(buflen);
+	if (buf == NULL) {
+		ERRS("no memory for buffer: %s\n");
+		return EXIT_FAILURE;
+	}
+	memset(buf, PADDING_BYTE, buflen);
+
+	/* Load kernel */
+	kernel = buf + HDR_LENGTH;
+	fread(kernel, klen, 1, in);
+
+	/* Write magic values and filler */
+	writel(buf, HDR_OFF_MAGIC1, board->magic);
+	writel(buf, HDR_OFF_MAGIC2, board->magic);
+	writel(buf, HDR_OFF_FILLER, 0);
+
+	/* Write header and image length */
+	writel(buf, HDR_OFF_HDRLEN, HDR_LENGTH);
+	writel(buf, HDR_OFF_IMAGELEN, klen);
+
+	/* Write checksum and static hash */
+	sha1_csum(kernel, klen, buf + HDR_OFF_CHECKSUM);
+	memcpy(buf + HDR_OFF_STATICHASH, board->statichash, 20);
+
+	rc = fwrite(buf, buflen, 1, out);
+
+	free(buf);
+
+	return rc == 1 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+static uint32_t crc32_table[256] = {
+	0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
+	0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91,
+	0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7,
+	0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC, 0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5,
+	0x3B6E20C8, 0x4C69105E, 0xD56041E4, 0xA2677172, 0x3C03E4D1, 0x4B04D447, 0xD20D85FD, 0xA50AB56B,
+	0x35B5A8FA, 0x42B2986C, 0xDBBBC9D6, 0xACBCF940, 0x32D86CE3, 0x45DF5C75, 0xDCD60DCF, 0xABD13D59,
+	0x26D930AC, 0x51DE003A, 0xC8D75180, 0xBFD06116, 0x21B4F4B5, 0x56B3C423, 0xCFBA9599, 0xB8BDA50F,
+	0x2802B89E, 0x5F058808, 0xC60CD9B2, 0xB10BE924, 0x2F6F7C87, 0x58684C11, 0xC1611DAB, 0xB6662D3D,
+	0x76DC4190, 0x01DB7106, 0x98D220BC, 0xEFD5102A, 0x71B18589, 0x06B6B51F, 0x9FBFE4A5, 0xE8B8D433,
+	0x7807C9A2, 0x0F00F934, 0x9609A88E, 0xE10E9818, 0x7F6A0DBB, 0x086D3D2D, 0x91646C97, 0xE6635C01,
+	0x6B6B51F4, 0x1C6C6162, 0x856530D8, 0xF262004E, 0x6C0695ED, 0x1B01A57B, 0x8208F4C1, 0xF50FC457,
+	0x65B0D9C6, 0x12B7E950, 0x8BBEB8EA, 0xFCB9887C, 0x62DD1DDF, 0x15DA2D49, 0x8CD37CF3, 0xFBD44C65,
+	0x4DB26158, 0x3AB551CE, 0xA3BC0074, 0xD4BB30E2, 0x4ADFA541, 0x3DD895D7, 0xA4D1C46D, 0xD3D6F4FB,
+	0x4369E96A, 0x346ED9FC, 0xAD678846, 0xDA60B8D0, 0x44042D73, 0x33031DE5, 0xAA0A4C5F, 0xDD0D7CC9,
+	0x5005713C, 0x270241AA, 0xBE0B1010, 0xC90C2086, 0x5768B525, 0x206F85B3, 0xB966D409, 0xCE61E49F,
+	0x5EDEF90E, 0x29D9C998, 0xB0D09822, 0xC7D7A8B4, 0x59B33D17, 0x2EB40D81, 0xB7BD5C3B, 0xC0BA6CAD,
+	0xEDB88320, 0x9ABFB3B6, 0x03B6E20C, 0x74B1D29A, 0xEAD54739, 0x9DD277AF, 0x04DB2615, 0x73DC1683,
+	0xE3630B12, 0x94643B84, 0x0D6D6A3E, 0x7A6A5AA8, 0xE40ECF0B, 0x9309FF9D, 0x0A00AE27, 0x7D079EB1,
+	0xF00F9344, 0x8708A3D2, 0x1E01F268, 0x6906C2FE, 0xF762575D, 0x806567CB, 0x196C3671, 0x6E6B06E7,
+	0xFED41B76, 0x89D32BE0, 0x10DA7A5A, 0x67DD4ACC, 0xF9B9DF6F, 0x8EBEEFF9, 0x17B7BE43, 0x60B08ED5,
+	0xD6D6A3E8, 0xA1D1937E, 0x38D8C2C4, 0x4FDFF252, 0xD1BB67F1, 0xA6BC5767, 0x3FB506DD, 0x48B2364B,
+	0xD80D2BDA, 0xAF0A1B4C, 0x36034AF6, 0x41047A60, 0xDF60EFC3, 0xA867DF55, 0x316E8EEF, 0x4669BE79,
+	0xCB61B38C, 0xBC66831A, 0x256FD2A0, 0x5268E236, 0xCC0C7795, 0xBB0B4703, 0x220216B9, 0x5505262F,
+	0xC5BA3BBE, 0xB2BD0B28, 0x2BB45A92, 0x5CB36A04, 0xC2D7FFA7, 0xB5D0CF31, 0x2CD99E8B, 0x5BDEAE1D,
+	0x9B64C2B0, 0xEC63F226, 0x756AA39C, 0x026D930A, 0x9C0906A9, 0xEB0E363F, 0x72076785, 0x05005713,
+	0x95BF4A82, 0xE2B87A14, 0x7BB12BAE, 0x0CB61B38, 0x92D28E9B, 0xE5D5BE0D, 0x7CDCEFB7, 0x0BDBDF21,
+	0x86D3D2D4, 0xF1D4E242, 0x68DDB3F8, 0x1FDA836E, 0x81BE16CD, 0xF6B9265B, 0x6FB077E1, 0x18B74777,
+	0x88085AE6, 0xFF0F6A70, 0x66063BCA, 0x11010B5C, 0x8F659EFF, 0xF862AE69, 0x616BFFD3, 0x166CCF45,
+	0xA00AE278, 0xD70DD2EE, 0x4E048354, 0x3903B3C2, 0xA7672661, 0xD06016F7, 0x4969474D, 0x3E6E77DB,
+	0xAED16A4A, 0xD9D65ADC, 0x40DF0B66, 0x37D83BF0, 0xA9BCAE53, 0xDEBB9EC5, 0x47B2CF7F, 0x30B5FFE9,
+	0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF,
+	0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94, 0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D
+};
+
+static inline uint32_t crc32_accumulate_8(uint32_t crc, uint8_t ch)
+{
+        return crc32_table[(crc ^ ch) & 0xff] ^ (crc >> 8);
+}
+
+static void crc32_csum(uint8_t *buf, size_t len)
+{
+	uint32_t crc;
+	size_t i;
+
+	crc = ~0;
+	for (i = 0; i < len; i += 4) {
+		crc = crc32_accumulate_8(crc, buf[i + 3]);
+		crc = crc32_accumulate_8(crc, buf[i + 2]);
+		crc = crc32_accumulate_8(crc, buf[i + 1]);
+		crc = crc32_accumulate_8(crc, buf[i]);
+	}
+	crc = ~crc;
+
+	writel(buf, OLD_HDR_OFF_CHECKSUM, crc);
+}
+
+
+static int meraki_old(struct board_info *board, size_t klen,
+		      FILE *out, FILE *in)
+{
+	unsigned char *kernel;
+	unsigned char *buf;
+	size_t buflen;
+	size_t kspace;
+
+	size_t rc;
+	buflen = board->imagelen;
+	kspace = buflen - OLD_HDR_LENGTH;
+
+	if (klen > kspace) {
+		ERR("kernel file is too big - max size: 0x%08lX\n", kspace);
+		return EXIT_FAILURE;
+	}
+
+	/* If requested, resize buffer to remove padding */
+	if (strip_padding)
+		buflen = klen + OLD_HDR_LENGTH;
+
+	/* Allocate and initialize buffer for final image */
+	buf = malloc(buflen);
+	if (buf == NULL) {
+		ERRS("no memory for buffer: %s\n");
+		return EXIT_FAILURE;
+	}
+	memset(buf, PADDING_BYTE, buflen);
+
+	/* Load kernel */
+	kernel = buf + OLD_HDR_LENGTH;
+	fread(kernel, klen, 1, in);
+
+	/* Write magic values and filler */
+	writel(buf, OLD_HDR_OFF_MAGIC1, board->magic);
+	writel(buf, OLD_HDR_OFF_FILLER0, 0);
+	writel(buf, OLD_HDR_OFF_FILLER1, 0);
+	writel(buf, OLD_HDR_OFF_FILLER2, 0);
+
+	/* Write load and kernel entry point address */
+	writel(buf, OLD_HDR_OFF_LOAD_ADDR, board->load_addr);
+	writel(buf, OLD_HDR_OFF_ENTRY, board->entry);
+
+	/* Write header and image length */
+	writel(buf, OLD_HDR_OFF_IMAGELEN, klen);
+
+	/* this gets replaced later, after the checksum has been calculated */
+	writel(buf, OLD_HDR_OFF_CHECKSUM, 0);
+
+	/* Write checksum */
+	crc32_csum(buf, klen + OLD_HDR_LENGTH);
+
+	rc = fwrite(buf, buflen, 1, out);
+
+	free(buf);
+
+	return rc == 1 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
 int main(int argc, char *argv[])
 {
 	int ret = EXIT_FAILURE;
-	long klen;
-	size_t kspace;
-	unsigned char *kernel;
-	size_t buflen;
-	unsigned char *buf;
-	bool strip_padding = false;
 	char *ofname = NULL, *ifname = NULL;
 	FILE *out, *in;
+	size_t klen;
 
 	progname = basename(argv[0]);
 
@@ -197,63 +399,27 @@ int main(int argc, char *argv[])
 		goto err;
 	}
 
-	buflen = board->imagelen;
-	kspace = buflen - HDR_LENGTH;
-
 	/* Get kernel length */
 	fseek(in, 0, SEEK_END);
 	klen = ftell(in);
 	rewind(in);
 
-	if (klen > kspace) {
-		ERR("file \"%s\" is too big - max size: 0x%08lX\n",
-		    ifname, kspace);
-		goto err_close_in;
-	}
-
-	/* If requested, resize buffer to remove padding */
-	if (strip_padding)
-		buflen = klen + HDR_LENGTH;
-
-	/* Allocate and initialize buffer for final image */
-	buf = malloc(buflen);
-	if (buf == NULL) {
-		ERRS("no memory for buffer: %s\n");
-		goto err_close_in;
-	}
-	memset(buf, PADDING_BYTE, buflen);
-
-	/* Load kernel */
-	kernel = buf + HDR_LENGTH;
-	fread(kernel, klen, 1, in);
-
-	/* Write magic values and filler */
-	writel(buf, HDR_OFF_MAGIC1, board->magic);
-	writel(buf, HDR_OFF_MAGIC2, board->magic);
-	writel(buf, HDR_OFF_FILLER, 0);
-
-	/* Write header and image length */
-	writel(buf, HDR_OFF_HDRLEN, HDR_LENGTH);
-	writel(buf, HDR_OFF_IMAGELEN, klen);
-
-	/* Write checksum and static hash */
-	sha1_csum(kernel, klen, buf + HDR_OFF_CHECKSUM);
-	memcpy(buf + HDR_OFF_STATICHASH, board->statichash, 20);
-
-	/* Save finished image */
 	out = fopen(ofname, "w");
 	if (out == NULL) {
 		ERRS("could not open \"%s\" for writing: %s", ofname);
-		goto err_free;
+		goto err_close_in;
 	}
-	fwrite(buf, buflen, 1, out);
 
-	ret = EXIT_SUCCESS;
+	switch (board->header_type) {
+	case MERAKI_NEW:
+		ret = meraki_new(board, klen, out, in);
+		break;
+	case MERAKI_OLD:
+		ret = meraki_old(board, klen, out, in);
+		break;
+	}
 
 	fclose(out);
-
-err_free:
-	free(buf);
 
 err_close_in:
 	fclose(in);
